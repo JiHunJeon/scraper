81fc3b087411027d5e411351cc5df2374bead95e
Jeonjihun <nin1212@hufa.ac.kr>
2016-01-21 17:53:41 +0900

    파싱하는 도중 log 보이도록함 하나의 배열에 전부 저장

--- parsers/NaverCafeParser.rb
+++ parsers/NaverCafeParser.rb
@@ -13,6 +13,7 @@ module CafeParser
 
 
   class NaverParserV1
+    SLEEPPAGES = 10 # sleep할 페이지 수
 
     def initialize
       @list = Array.new()
@@ -27,15 +28,15 @@ module CafeParser
       #
       @list.push(@cafe_list)
 
-      # @cafe_list = Cafe.new()
-      # #dummy
-      # @cafe_list.name = "openuniversity"
-      # @cafe_list.current_page = 1
-      # @cafe_list.page = 2
-      # @cafe_list.url =
-      #     "http://cafe.naver.com/ArticleList.nhn?search.boardtype=L&search.questionTab=A&search.clubid=23161524&search.totalCount=151&userDisplay=50&noticeHidden=true&search.page="
-      # #
-      # @list.push(@cafe_list)
+      @cafe_list = Cafe.new()
+      #dummy
+      @cafe_list.name = "openuniversity"
+      @cafe_list.current_page = 1
+      @cafe_list.page = 2
+      @cafe_list.url =
+          "http://cafe.naver.com/ArticleList.nhn?search.boardtype=L&search.questionTab=A&search.clubid=23161524&search.totalCount=151&userDisplay=50&noticeHidden=true&search.page="
+      #
+      @list.push(@cafe_list)
       @mechanize =
           Mechanize.new {|a| a.ssl_version, a.verify_mode ='TLSv1',OpenSSL::SSL::VERIFY_NONE}
       @mechanize.user_agent_alias = 'Mac Safari'
@@ -44,52 +45,33 @@ module CafeParser
     def user_name_list
       name_list = Array.new()
       @list.each do |list|
-        name_list =  get_list(list,:user_name)
+        name_list.concat(get_list(list,:user_name)).uniq
       end
-      name_list.uniq
-    end
-
-    def user_name(url,current_page)
-      a = page(url,current_page)
-      a.encoding = "euc-kr"
-      view = a.css("td.p-nick a").to_a
-
-      user_name = view.map do |v|
-        id = v["onclick"].split(",")
-        id[3].gsub!(/[^0-9A-Za-zㄱ-ㅎㅏ-ㅣ가-힣.\-]/, '')
-      end
-      user_name.uniq
+      name_list
     end
 
     def user_id_list
       id_list = Array.new()
       @list.each do |list|
-        id_list =  get_list(list,:user_id)
+        id_list.concat(get_list(list,:user_id)).uniq
       end
-      id_list.uniq
-    end
-
-    def user_id(url,current_page)
-      view = page(url,current_page).css("td.p-nick a").to_a
-
-      user_id = view.map do |v|
-        id = v["onclick"].split(",")
-        id[1].gsub!(/[^0-9A-Za-z.\-]/, '')
-      end
-      user_id.uniq
+      id_list
     end
 
     # to sleep during 1second after get list.
     def get_list(list,type)
-      # puts list.name
-      # puts list.url
       tmp_list = Array.new()
+      puts list.name
+      puts list.url
       while list.current_page <= list.page
-        if list.current_page%10 != 0
-          # puts list.current_page
+        if list.current_page%SLEEPPAGES != 0
+          puts list.current_page
           if NaverParserV1.new.respond_to?(type)
             list_info = [list.url,list.current_page]
-            tmp_list.push(NaverParserV1.new.send(type, *list_info))
+            tmp_list.concat(NaverParserV1.new.send(type, *list_info)).uniq
+
+            puts tmp_list.count
+            puts "========================================="
           else
             puts "dosen't exit method"
             break
@@ -99,7 +81,29 @@ module CafeParser
           sleep 1
         end
       end
-      tmp_list.uniq
+      tmp_list
+    end
+
+    def user_name(url,current_page)
+      raw = page(url,current_page)
+      raw.encoding = "euc-kr"
+      extraction = raw.css("td.p-nick a").to_a
+
+      user_name = extraction.map do |v|
+        id = v["onclick"].split(",")
+        id[3].gsub!(/[^0-9A-Za-zㄱ-ㅎㅏ-ㅣ가-힣.\-]/, '')
+      end
+      user_name.uniq
+    end
+
+    def user_id(url,current_page)
+      extraction = page(url,current_page).css("td.p-nick a").to_a
+
+      user_id = extraction.map do |v|
+        id = v["onclick"].split(",")
+        id[1].gsub!(/[^0-9A-Za-z.\-]/, '')
+      end
+      user_id.uniq
     end
 
     def page(url,current_page)
