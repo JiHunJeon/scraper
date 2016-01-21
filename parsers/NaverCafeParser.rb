d653b045909b6b19b72514f920b6561150576779
Jeonjihun <nin1212@hufa.ac.kr>
2016-01-21 16:17:43 +0900

    naver parser name list id list 불러오기 완성

--- parsers/NaverCafeParser.rb
+++ parsers/NaverCafeParser.rb
@@ -1,5 +1,6 @@
 module CafeParser
   class NaverCafeParser < ParserUsage
+    attr_accessor :list
 
     def self.can_parse?(site)
       true
@@ -14,31 +15,42 @@ module CafeParser
   class NaverParserV1
 
     def initialize
+      @list = Array.new()
+
       @cafe_list = Cafe.new()
       #dummy
       @cafe_list.name = "specup"
-      @cafe_list.current_page = "1"
-      @cafe_list.page = "10"
+      @cafe_list.current_page = 1
+      @cafe_list.page = 2
       @cafe_list.url =
           "http://cafe.naver.com/ArticleList.nhn?search.boardtype=L&search.questionTab=A&search.clubid=15754634&search.totalCount=151&userDisplay=50&noticeHidden=true&search.page="
       #
+      @list.push(@cafe_list)
+
+      # @cafe_list = Cafe.new()
+      # #dummy
+      # @cafe_list.name = "openuniversity"
+      # @cafe_list.current_page = 1
+      # @cafe_list.page = 2
+      # @cafe_list.url =
+      #     "http://cafe.naver.com/ArticleList.nhn?search.boardtype=L&search.questionTab=A&search.clubid=23161524&search.totalCount=151&userDisplay=50&noticeHidden=true&search.page="
+      # #
+      # @list.push(@cafe_list)
       @mechanize =
           Mechanize.new {|a| a.ssl_version, a.verify_mode ='TLSv1',OpenSSL::SSL::VERIFY_NONE}
       @mechanize.user_agent_alias = 'Mac Safari'
     end
 
-    def user_id
-      view = page.css("td.p-nick a").to_a
-
-      user_id = view.map do |v|
-        id = v["onclick"].split(",")
-        id[1].gsub!(/[^0-9A-Za-z.\-]/, '')
+    def user_name_list
+      name_list = Array.new()
+      @list.each do |list|
+        name_list =  get_list(list,:user_name)
       end
-      puts user_id.uniq
+      name_list.uniq
     end
 
-    def user_name
-      a = page
+    def user_name(url,current_page)
+      a = page(url,current_page)
       a.encoding = "euc-kr"
       view = a.css("td.p-nick a").to_a
 
@@ -50,27 +62,48 @@ module CafeParser
     end
 
     def user_id_list
-      @cafe_list.each do |list|
-        puts list.url
+      id_list = Array.new()
+      @list.each do |list|
+        id_list =  get_list(list,:user_id)
       end
+      id_list.uniq
     end
 
-    def page()
-      @mechanize.get("#{@cafe_list.url}"+"#{@cafe_list.current_page}")
+    def user_id(url,current_page)
+      view = page(url,current_page).css("td.p-nick a").to_a
+
+      user_id = view.map do |v|
+        id = v["onclick"].split(",")
+        id[1].gsub!(/[^0-9A-Za-z.\-]/, '')
+      end
+      user_id.uniq
     end
 
-    def all_posts
-      puts page.css("span.p11 strong").text
+    # to sleep during 1second after get list.
+    def get_list(list,type)
+      # puts list.name
+      # puts list.url
+      tmp_list = Array.new()
+      while list.current_page <= list.page
+        if list.current_page%10 != 0
+          # puts list.current_page
+          if NaverParserV1.new.respond_to?(type)
+            list_info = [list.url,list.current_page]
+            tmp_list.push(NaverParserV1.new.send(type, *list_info))
+          else
+            puts "dosen't exit method"
+            break
+          end
+          list.current_page += 1
+        else
+          sleep 1
+        end
+      end
+      tmp_list.uniq
     end
 
-
-    def last_page
-      @cafe_list.current_page = '2'
+    def page(url,current_page)
+      @mechanize.get("#{url}"+"#{current_page}")
     end
-
-    def all_pages
-      all_pages_number =  all_post_number / 50
-    end
-
   end
 end
